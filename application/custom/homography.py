42ef78f01bbf767fc8690e21b94b4228cab220cc
Ryan Eggert <RyanEggertWI@gmail.com>
2016-04-04 21:28:06 -0700

    Detecting clicks on existing points.

--- application/custom/homography.py
+++ application/custom/homography.py
@@ -30,14 +30,46 @@ class HomographyScene(QtGui.QGraphicsScene):
         self.points = []
 
         # Point configuration
-        self.point_rad = 5
+        self.point_rad = 5  # Radius, in pixels
         self.point_pen = QtGui.QPen()
-        self.point_brush_color = QtGui.QColor(255, 25, 23)
+        self.point_brush_color = QtGui.QColor(255, 25, 23)  # R, G, B
         self.point_brush = QtGui.QBrush(self.point_brush_color)
 
     def mouseReleaseEvent(self, event):
-        super(HomographyScene, self).mousePressEvent(event)
+        super(HomographyScene, self).mouseReleaseEvent(event)
         loc = (event.scenePos().x(), event.scenePos().y())
         print(event.scenePos().x(), event.scenePos().y())
-        new_point = self.addEllipse(loc[0], loc[1], self.point_rad * 2, self.point_rad * 2, self.point_pen, self.point_brush)
+        new_point = self.addEllipse(loc[0] - self.point_rad, loc[1] - self.point_rad, self.point_rad * 2, self.point_rad * 2, self.point_pen, self.point_brush)
         self.points.append(new_point)
+
+    def mousePressEvent(self, event):
+        super(HomographyScene, self).mousePressEvent(event)
+        loc = (event.scenePos().x(), event.scenePos().y())
+        clicked = self.find_clicked_point(loc)
+        print clicked
+
+    def find_clicked_point(self, click_loc):
+        """
+        Searches placed points and returns a point if the user has clicked it.
+        Else returns False.
+        """
+        for point in self.points:
+            # if self.click_is_within(point.rect(), click_loc):
+            if point.rect().contains(click_loc[0], click_loc[1]):
+                return point
+        return False
+
+    @staticmethod
+    def click_is_within(ellipse_rect, click):
+        """
+        Returns True if click (x, y) is within the rectange of a drawn ellipse.
+        Else returns False. Use this if we need to manually specify a selection radius.
+        """
+        min_x, min_y, max_x, max_y = ellipse_rect.getCoords()  # Coordinates of bounding rectangle.
+        if click[0] > min_x and click[0] < max_x:  # If click within x-bounds
+            if click[1] > min_y and click[1] < max_y:  # and click within y-bounds
+                return True
+            else:
+                return False
+        else:
+            return False
