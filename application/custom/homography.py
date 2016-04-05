c7576df434894d2460e8fa3ecf686e6cb4091706
Ryan Eggert <RyanEggertWI@gmail.com>
2016-04-04 19:52:32 -0700

    Click to add points to image.

--- application/custom/homography.py
+++ application/custom/homography.py
@@ -14,11 +14,8 @@ class HomographyView(QtGui.QGraphicsView):
         this HomographyView's scene. The image's top left corner will
         be placed at (0,0) in the scene.
         """
-        # im = QtGui.QImage(filename)
-        # im = QtGui.QImage("stmarc_image.png")
         new_scene = HomographyScene(self)
         new_scene.addPixmap(QtGui.QPixmap().fromImage(image))
-        # this.drawBackground = draw_image
         self.setScene(new_scene)
         self.show()
 
@@ -32,6 +29,15 @@ class HomographyScene(QtGui.QGraphicsScene):
         super(HomographyScene, self).__init__(parent)
         self.points = []
 
+        # Point configuration
+        self.point_rad = 5
+        self.point_pen = QtGui.QPen()
+        self.point_brush_color = QtGui.QColor(255, 25, 23)
+        self.point_brush = QtGui.QBrush(self.point_brush_color)
+
     def mouseReleaseEvent(self, event):
         super(HomographyScene, self).mousePressEvent(event)
+        loc = (event.scenePos().x(), event.scenePos().y())
         print(event.scenePos().x(), event.scenePos().y())
+        new_point = self.addEllipse(loc[0], loc[1], self.point_rad * 2, self.point_rad * 2, self.point_pen, self.point_brush)
+        self.points.append(new_point)
