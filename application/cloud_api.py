3ee2e62cde2e2e4ed27324380c42a42b3738707a
Philip Seger <seger.philip@gmail.com>
2017-02-06 21:47:43 -0500

    EDIT: removed comment

--- application/cloud_api.py
+++ application/cloud_api.py
@@ -245,7 +245,6 @@ class CloudWizard:
         payload = {
             'identifier': identifier,
         }
-        # TODO will be a zip shortly
         local_filename = 'results.zip'
         r = requests.get(self.server_addr + 'retrieveResults', data = payload, stream=True)
         with open(local_filename, 'wb') as f:
