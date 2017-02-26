2794e2d2894c348771639c103aec4b9217834195
Philip Seger <seger.philip@gmail.com>
2017-02-26 14:21:41 -0500

    EDIT: proper opencv version now

--- install_conda_deps.sh
+++ install_conda_deps.sh
@@ -1,6 +1,6 @@
 YOURENVNAME=$1
 conda install -n $YOURENVNAME Pillow=2.7.0
 conda install -n $YOURENVNAME requests=2.13.0
-conda install -n $YOURENVNAME opencv=3.1
+conda install -n $YOURENVNAME -c menpo opencv=2.4.11
 conda install -n $YOURENVNAME pyqt=5.6.0
 conda install -n $YOURENVNAME qtawesome=0.4.4
