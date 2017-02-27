dbcd37352c287adc8315f03ce7b72df25eb623a9
Deniz Celik <dcelik@users.noreply.github.com>
2017-02-27 12:12:24 -0500

    Update install_conda_deps.sh

--- install_conda_deps.sh
+++ install_conda_deps.sh
@@ -4,3 +4,4 @@ conda install -n $YOURENVNAME requests=2.13.0
 conda install -n $YOURENVNAME -c menpo opencv=2.4.11
 conda install -n $YOURENVNAME pyqt=5.6.0
 conda install -n $YOURENVNAME qtawesome=0.4.4
+conda install -n $YOURENVNAME -c conda-forge requests-toolbelt=0.7.1
