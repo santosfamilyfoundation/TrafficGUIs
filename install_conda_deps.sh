7c880e6cc4abe1f1654e1ca152e442eee4c2c44e
Jacob Riedel <jr112394@gmail.com>
2017-03-01 11:55:29 -0500

    Version Numbers

--- install_conda_deps.sh
+++ install_conda_deps.sh
@@ -4,9 +4,9 @@ conda install -n $YOURENVNAME requests=2.13.0
 conda install -n $YOURENVNAME -c menpo opencv=2.4.11
 conda install -n $YOURENVNAME pyqt=5.6.0
 conda install -n $YOURENVNAME qtawesome=0.4.4
-conda install -n $YOURENVNAME nomkl numpy
-conda install -n $YOURENVNAME nomkl scipy
-conda install -n $YOURENVNAME nomkl scikit-learn
-conda install -n $YOURENVNAME nomkl numexpr
+conda install -n $YOURENVNAME nomkl numpy=1.12.0
+conda install -n $YOURENVNAME nomkl scipy=0.18.1
+conda install -n $YOURENVNAME nomkl scikit-learn=0.18.1
+conda install -n $YOURENVNAME nomkl numexpr=2.6.2
 conda remove -n $YOURENVNAME mkl mkl-service
 conda install -n $YOURENVNAME -c conda-forge requests-toolbelt=0.7.1
