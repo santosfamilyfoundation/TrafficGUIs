823a7f5914474964a14e4f57328ab3f2bfa62624
Jacob Riedel <jr112394@gmail.com>
2017-03-01 09:15:57 -0500

    Moved files to packaging folder

--- application/packaging/SantosBuild.sh
+++ application/packaging/SantosBuild.sh
@@ -2,10 +2,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo $DIR
-
 cd $DIR
-pyinstaller $DIR/app.spec
+cd ..
+echo $DIR
+pyinstaller -distpath=$DIR -workpath=$DIR $DIR/app.spec
 chmod 777 $DIR/dist/app
 
 echo “[Press any button to exit]”
--- install_conda_deps.sh
+++ install_conda_deps.sh
@@ -4,6 +4,9 @@ conda install -n $YOURENVNAME requests=2.13.0
 conda install -n $YOURENVNAME -c menpo opencv=2.4.11
 conda install -n $YOURENVNAME pyqt=5.6.0
 conda install -n $YOURENVNAME qtawesome=0.4.4
-conda install -n $YOURENVNAME nomkl numpy scipy scikit-learn numexpr
+conda install -n $YOURENVNAME nomkl numpy
+conda install -n $YOURENVNAME nomkl scipy
+conda install -n $YOURENVNAME nomkl scikit-learn
+conda install -n $YOURENVNAME nomkl numexpr
 conda remove -n $YOURENVNAME mkl mkl-service
 conda install -n $YOURENVNAME -c conda-forge requests-toolbelt=0.7.1
