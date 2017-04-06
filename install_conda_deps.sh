33f2e7ac564d0d6d0f01d9df80fa2789fb976a9b
Sawyer Vaughan <sawyerv726@gmail.com>
2017-04-06 13:14:17 -0700

    Rename envs/ folder

--- install_conda_deps.sh
+++ install_conda_deps.sh
@@ -6,12 +6,12 @@ conda remove -y --all -n $YOURENVNAME
 if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "darwin"* ]]; then
 	# Linux or macOS install
 	echo "Detected Unix; Installing dependencies..."
-	conda env create -f install/env_unix.yml -n $YOURENVNAME
+	conda env create -f envs/env_unix.yml -n $YOURENVNAME
 
 elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
 	# Some form of windows
 	echo "Detected Windows; Installing dependencies..."
-	conda env create -f install/env_windows.yml -n $YOURENVNAME
+	conda env create -f envs/env_windows.yml -n $YOURENVNAME
 
 else
 	# Unknown.
