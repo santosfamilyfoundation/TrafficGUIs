9ac54187d566661e8b125923fc2a2b81f17b9943
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2017-04-06 15:55:01 -0400

    Revamp install instructions for Windows

--- install_conda_deps.sh
+++ install_conda_deps.sh
@@ -1,5 +1,23 @@
+
 YOURENVNAME=$1
 
-conda env create -f santos_gui_env.yml -n YOURENVNAME
+if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "darwin"* ]]; then
+	# Linux or macOS install
+	echo "Detected Unix; Installing dependencies..."
+	conda env create -f install/env_unix.yml -n $YOURENVNAME
+
+elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
+	# Some form of windows
+	echo "Detected Windows; Installing dependencies..."
+	conda env create -f install/env_windows.yml -n $YOURENVNAME
+
+else
+	# Unknown.
+	echo "Unknown OS; You will have to install dependencies manually."
+	exit 1
+
+fi
+
 source deactivate
-source activate YOURENVNAME
+source activate $YOURENVNAME
+
